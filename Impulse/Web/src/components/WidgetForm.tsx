9ae4fab6e2daede3cbaf6998e36e5b4567640e73
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-05-06 16:54:37 -0300

    Atribuindo tipos aos campos de seleção de feedback

--- Impulse/Web/src/components/WidgetForm.tsx
+++ Impulse/Web/src/components/WidgetForm.tsx
@@ -31,15 +31,11 @@ const feedbackTypes = {
 type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-    const [feedbackType,setFeedbackType] = useState<FeedbackType | null>(null)
-    
-    
-    
+    const [feedbackType,setFeedbackType] = useState<FeedbackType | null>(null);       
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
-
                 <CloseButton />
             </header>
             
@@ -63,10 +59,8 @@ export function WidgetForm() {
                 <p>Hello World</p>
             )}
          
-
-
             <footer className="text-xs text-neutral-400">
-                Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+                Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/GuilhermeSantiago25">Guilherme Santiago</a>
             </footer>
         </div>
 
